6609cd97b0bc5b786d1434638dfa846c92a27c5a
matvlaale <maculaali@yandex.ru>
2020-04-10 14:44:10 +0300

    Lesson_3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	    int lol = 1;
-        System.out.println(lol);
+	    int kol = 1;
+        System.out.println(kol);
     }
 }

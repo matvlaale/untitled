afca3c5e8972c1103e95283a9d5c7c7ec928769b
matvlaale <maculaali@yandex.ru>
2020-04-12 23:15:43 +0300

    Try_2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 	    int kol = 1;
         System.out.println(kol);
+        scanner.close();
     }
 }
